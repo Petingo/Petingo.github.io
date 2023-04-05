9dc757e35d4b2f3082013e6fa767391d3043117b
Petingo <eyanghongbin0315@gmail.com>
2023-04-05 02:19:31 +0200

    20230405

--- assets/js/ext.js
+++ assets/js/ext.js
@@ -5,9 +5,9 @@ new TypeIt("#typeit-block", {
     delay: 700,
     loop: true
 })
-    .type("<span style='color: #F17C67'>Computer Science</span>", {delay: 700})
+    .type("<span style='color: #F17C67'>Computer Vision</span>", {delay: 700})
     .delete(16)
     .type("<span style='color: #BEC23F'>Photography</span>", {delay: 700})
-    .delete(11)
-    .type("<span style='color: #0089A7'>Architecture</span>", {delay: 700})
+    .delete(16)
+    .type("<span style='color: #0089A7'>Digital Fabrication</span>", {delay: 700})
     .go();
